1766f0a6b2881f703ce6aaebe75edb26004d5a2b
kernkw <kyle.w.kern@gmail.com>
2013-08-16 13:00:18 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZendeskUserStats::Application.config.secret_token = 'dc09a67f36569abbcd63b81b5fad165ea441940ac73ae00d2825d509399f0e2fe0917c6e8c960c79bf933cec7195cf028a390edeb768db126cc61d8ee2e79bf8'
