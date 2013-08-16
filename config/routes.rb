97ac51bcdbcd6eb53db60347708c1a9584de47da
kernkw <kyle.w.kern@gmail.com>
2013-08-16 14:53:28 -0600

    pushing again

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 ZendeskUserStats::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/login"
-  get "static_pages/logout"
-  get "static_pages/about"
-  get "static_pages/contact"
-  
+match '/index' => 'static_pages#index'
+  get "static_pages/login.html"
 end
